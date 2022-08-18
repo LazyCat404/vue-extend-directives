7dab5f7a7003ec3bfc1738bbc0fa55590378ed8f
LazyCat404 <wenjie_address@outlook.com>
2022-08-18 14:42:03 +0800

    依赖更新及优化

--- packages/index.ts
+++ packages/index.ts
@@ -3,10 +3,14 @@ interface dir {
   name: string; // 指令名
   dir: any; // 指令方法
 }
+interface Module {
+  default:dir
+}
 export const customDirective: dir[] = [];
-const modulesFiles = import.meta.globEager('./**/index.ts');
+const modulesFiles = import.meta.glob('./**/index.ts',{ eager: true });
+
 for (const path in modulesFiles) {
-  customDirective.push(modulesFiles[path].default);
+  customDirective.push((modulesFiles[path] as Module).default);
 }
 
 // 指令注册
